c5bd261a511d05700133b3a8be525f6c66891e8a
Ian Bishop <bishop14@gmail.com>
2013-10-18 16:21:11 -0300

    Fix #4

--- config/default.rb
+++ config/default.rb
@@ -1,6 +1,6 @@
 module GoSquared
   API_ENDPOINT = "https://api.gosquared.com"
-  EVENT_ENDPOINT = "https://data.gosquared.com/event"
+  EVENT_ENDPOINT = "https://data.gosquared.com"
   API_FUNCTIONS = [
     "aggregateStats",
     "campaigns",
